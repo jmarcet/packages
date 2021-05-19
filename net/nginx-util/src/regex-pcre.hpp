7bfa24844bfd418169273b72ff60f47b0a45d790
Peter Stadler <peter.stadler@student.uibk.ac.at>
2021-05-19 10:38:44 +0200

    nginx-util: do not use fallthrough attribute
    fixes issue #15653
    
    Signed-off-by: Peter Stadler <peter.stadler@student.uibk.ac.at>
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- net/nginx-util/src/regex-pcre.hpp
+++ net/nginx-util/src/regex-pcre.hpp
@@ -289,7 +289,7 @@ auto smatch::format(const std::string& fmt) const
                 }  // else:
 
                 ret += '$';
-                [[fallthrough]];
+                /* fallthrough */
 
             case '$':  // escaped
                 ret += chr;
