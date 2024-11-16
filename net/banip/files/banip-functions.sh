46ac9a8cf424a54ff4c4bff502adc1b5752c0917
Javier Marcet <javier@marcet.info>
2024-11-16 13:45:11 +0100

    Revert "banIP: update to 1.0.0-10"
    This reverts commit 4f00d8f0ee66effd40ec7c56a5a9a7395a7a8d54.
    
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- net/banip/files/banip-functions.sh
+++ net/banip/files/banip-functions.sh
@@ -360,8 +360,10 @@ f_actual() {
 f_getfetch() {
 	local item utils insecure update="0"
 
-	if [ -n "${ban_fetchcmd}" ]; then
-		ban_fetchcmd="$(f_cmd "${ban_fetchcmd}" optional)"
+	if { [ "${ban_fetchcmd}" = "uclient-fetch" ] && printf "%s" "${ban_packages}" | "${ban_grepcmd}" -q '"libustream-'; } ||
+		{ [ "${ban_fetchcmd}" = "wget" ] && printf "%s" "${ban_packages}" | "${ban_grepcmd}" -q '"wget-ssl'; } ||
+		[ "${ban_fetchcmd}" = "curl" ] || [ "${ban_fetchcmd}" = "aria2c" ]; then
+		ban_fetchcmd="$(f_cmd "${ban_fetchcmd}" "true")"
 	fi
 
 	if [ "${ban_autodetect}" = "1" ] && [ ! -x "${ban_fetchcmd}" ]; then
