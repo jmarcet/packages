d6d79604377bd7666eac482d6cce86e764ac2c32
Javier Marcet <javier@marcet.info>
2025-03-29 15:50:39 +0100

    Revert "banIP: release 1.5.5-1"
    This reverts commit 716e73da56fbe6c10b758b5024779872f1a1a581.
    
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- net/banip/files/banip-service.sh
+++ net/banip/files/banip-service.sh
@@ -53,6 +53,7 @@ for feed in allowlist ${ban_feed} blocklist; do
 	#
 	if [ "${feed}" = "allowlist" ] || [ "${feed}" = "blocklist" ]; then
 		for proto in 4MAC 6MAC 4 6; do
+			[ "${feed}" = "blocklist" ] && wait
 			f_down "${feed}" "${proto}" "-" "-" "inout"
 		done
 		continue
@@ -98,7 +99,8 @@ for feed in allowlist ${ban_feed} blocklist; do
 				f_down "${feed}" "4" "${feed_url_4}" "${feed_rule_4}" "${feed_chain:-"in"}" "${feed_flag}"
 			else
 				(f_down "${feed}" "4" "${feed_url_4}" "${feed_rule_4}" "${feed_chain:-"in"}" "${feed_flag}") &
-				[ "${cnt}" -gt "${ban_cores}" ] && wait -n
+				hold="$((cnt % ban_cores))"
+				[ "${hold}" = "0" ] && wait -n
 				cnt="$((cnt + 1))"
 			fi
 		fi
@@ -114,12 +116,12 @@ for feed in allowlist ${ban_feed} blocklist; do
 			done
 		else
 			(f_down "${feed}" "6" "${feed_url_6}" "${feed_rule_6}" "${feed_chain:-"in"}" "${feed_flag}") &
-			[ "${cnt}" -gt "${ban_cores}" ] && wait -n
 			cnt="$((cnt + 1))"
+			hold="$((cnt % ban_cores))"
+			[ "${hold}" = "0" ] && wait -n
 		fi
 	fi
 done
-wait
 f_rmset
 f_rmdir "${ban_tmpdir}"
 f_genstatus "active"
@@ -130,7 +132,8 @@ f_log "info" "start banIP domain lookup"
 cnt="1"
 for list in allowlist blocklist; do
 	(f_lookup "${list}") &
-	[ "${cnt}" -gt "${ban_cores}" ] && wait -n
+	hold="$((cnt % ban_cores))"
+	[ "${hold}" = "0" ] && wait -n
 	cnt="$((cnt + 1))"
 done
 wait
